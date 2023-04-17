2f81bed45164f46fee65d996f88a42cd45a79d1a
Christopher Aguiar <chrisaguiar72@gmail.com>
2023-04-17 16:55:46 -0400

    API Working

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Do nothing
+function getPrice() {
+    const symbol = document.getElementById("symbol").value;
+    const apiKey = "5Eh4KkNVkaBFWowAUy4z6WTO_pe1J_Iq";
+    const apiUrl = `https://api.polygon.io/v2/aggs/ticker/${symbol}/prev?apiKey=${apiKey}`;
+    
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const price = data.results[0].c;
+        document.getElementById("price").innerHTML = `Last close price: $${price}`;
+      })
+      .catch(error => console.log(error));
+  }
+  
