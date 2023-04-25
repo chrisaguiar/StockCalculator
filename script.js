6a6c558b5f53d5dbe55a232b71fba58a14812d06
Christopher Aguiar <chrisaguiar72@gmail.com>
2023-04-25 16:20:56 -0400

    Merged Changes from API and website

--- script.js
+++ script.js
@@ -6,8 +6,16 @@ function getPrice() {
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
-        const price = data.results[0].c;
-        document.getElementById("price").innerHTML = `Last close price: $${price}`;
+        const closePrice = data.results[0].c;
+        const openPrice = data.results[0].o;
+        const volume = data.results[0].v;
+        const high = data.results[0].h;
+        const low = data.results[0].l;
+        document.getElementById("openPrice").innerHTML = `${symbol}'s last opening price was: $${openPrice}`;
+        document.getElementById("closePrice").innerHTML = `${symbol}'s last closing price was: $${closePrice}`;
+        document.getElementById("Volume").innerHTML = `${symbol}'s trading volume was: ${volume}`;
+        document.getElementById("High").innerHTML = `${symbol}'s highest price was: $${high}`;
+        document.getElementById("Low").innerHTML = `${symbol}'s lowest price was: $${low}`;
       })
       .catch(error => console.log(error));
   }
